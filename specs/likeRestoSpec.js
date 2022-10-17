66fbb5ad10dd2d7973374b6cca0991fb6e99f24e
arifrizqi <arifrizqy.ar15@gmail.com>
2022-10-17 10:45:21 +0700

    berhasil membuat fitur tes like dan unlike

--- specs/likeRestoSpec.js
+++ specs/likeRestoSpec.js
@@ -59,7 +59,7 @@ describe('Liking A Movie', () => {
     FavoriteRestaurantIdb.deleteRestaurant(1);
   });
 
-  xit('should not add a restaurant when it has no id', async () => {
+  it('should not add a restaurant when it has no id', async () => {
     await LikeButtonInitiator.init({
       likeButtonContainer: document.querySelector('#likeButtonContainer'),
       restaurant: {},
